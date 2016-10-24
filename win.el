3601651e395e10ba510c792f42e64c4f9acfa311
showgood <showgood21@gmail.com>
2016-10-24 11:27:45 +1100

    more updates

--- win.el
+++ win.el
@@ -1,4 +1,12 @@
 (load (concat dotspacemacs-directory "common.el"))
 (load (concat dotspacemacs-directory "eshell.win.el"))
 (load (concat dotspacemacs-directory "tramp.win.el"))
+(setq eshell-directory-name (concat dotspacemacs-directory "eshell_win"))
 (setq magit-git-executable "c:/Program Files (x86)/GIT/cmd/git.exe")
+
+;; (defun cygwin-shell ()
+;;   "Run cygwin bash in shell mode."
+;;   (interactive)
+;;   "tab completion doesn't work in zsh"
+;;   (let ((explicit-shell-file-name "C:/cygwin/bin/bash"))
+;;     (call-interactively 'shell)))
--- windows.el
+++
@@ -1,9 +0,0 @@
-(defun cygwin-shell ()
-  "Run cygwin bash in shell mode."
-  (interactive)
-  "tab completion doesn't work in zsh"
-  (let ((explicit-shell-file-name "C:/cygwin/bin/bash"))
-    (call-interactively 'shell)))
-
-(setq magit-git-executable "C:/Users/xiaomingw/AppData/Local/Programs/Git/cmd/git")
-;; (load "~/dotspacemacs/gnus.office.el")
